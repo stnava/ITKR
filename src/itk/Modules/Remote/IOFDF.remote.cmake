f71ccbfe4362b7084ffe669a83ea0123fbc676d2
stnava <stnava@gmail.com>
2015-03-20 17:38:57 -0400

    WIP:...

--- src/itk/Modules/Remote/IOFDF.remote.cmake
+++
@@ -1,5 +0,0 @@
-itk_fetch_module(IOFDF
-  "FDFImageIO plugin for ITK. Authors Gleen Pierce/Nick Tustison/Kent Williams"
-  GIT_REPOSITORY http://github.com/InsightSoftwareConsortium/itkFDFImageIO.git
-  GIT_TAG  0805a5e7ff0bd60cf3e5e59830223a62dc988d2e
-  )
